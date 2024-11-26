317c0a5bbbe0992baf3b2c38d60debdc368a7117
Jillian Armonia <jillian.marice@gmail.com>
2024-11-26 20:18:47 +0900

    add new fruits when moved

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ let fruit = {
     y: null,
 }
 
+let maxIndex = 0;
+
 document.addEventListener('mousedown', (event) => {
     if (event.target.classList.contains('fruit')){
         cursor = {
@@ -20,7 +22,14 @@ document.addEventListener('mousedown', (event) => {
             x: event.target.getBoundingClientRect().left,
             y: event.target.getBoundingClientRect().top
         }
-        console.table(fruit)
+
+        if (event.target.classList.contains('moved') === false){
+            event.target.classList.add('moved');
+            event.target.parentNode.click();
+            event.target.style.fontSize = '70px'
+        }
+
+        event.target.style.zIndex = `${maxIndex += 1}`
     }
 })
 
@@ -42,4 +51,28 @@ document.addEventListener('mousemove', (event) => {
 document.addEventListener('mouseup', () => {
     fruit.dom = null
     fruit.dom.style.cursor = 'auto';
+})
+
+//Create a pseudocode to create a new fruit when the button is clicked
+
+//CLICK the fruit button
+    //CREATE a new div element (newFruit)
+    //ADD a class attribute of 'fruit'
+    //IF the button id is strawberry
+        //THEN put a strawberry in the div html
+    //APPEND the newFruit to the button
+
+document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('fruitBtn')){
+        let newFruit = document.createElement('div');
+        newFruit.classList.add('fruit');
+
+        switch (event.target.id){
+            case 'strawberry':
+                newFruit.innerHTML = '🍓';
+                break;
+        }
+
+        event.target.append(newFruit)
+    }
 })
